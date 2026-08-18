f4be9e4584de7e217360920c3366839bfa7eb564
cantera-gt <businesscatserrano@gmail.com>
2026-08-18 15:48:45 +0200

    Add remote patterns for image optimization

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,13 @@ const nextConfig: NextConfig = {
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wlxgvbabljflvhtxuzue.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
   },
 };
 
